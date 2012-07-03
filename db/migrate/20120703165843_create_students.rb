0fa498c3a44c0084ef116cfde159d9603998df20
mslaughter <melissaslaughter@me.com>
2012-07-03 18:01:19 -0300

    change pinned_table from integer to string

--- db/migrate/20120703165843_create_students.rb
+++ db/migrate/20120703165843_create_students.rb
@@ -4,7 +4,7 @@ class CreateStudents < ActiveRecord::Migration
       t.integer   :classroom_id
       t.string    :name
       t.boolean   :pin, default: false
-      t.integer   :pinned_table
+      t.string    :pinned_table
       t.string    :students_sat_next_to, default: ""
       
       t.timestamps
